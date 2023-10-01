ba741171d15c14030d65f64c6ee0fd687586e05e
Abrorjon Asralov <113268353+asralov@users.noreply.github.com>
2023-09-30 21:48:41 -0700

    Updated number_guesser.py
    I have modified the code and completely changed the design of my code, it was a good experience to see the previous code that has been staying here for 9 months or so, and I have updated and changed the design of code so it looks more acceptable by me now.

--- number_guesser.py
+++ number_guesser.py
@@ -1,50 +1,66 @@
-# Full_name: Abrorjon Asralov
+__author__ = "Abrorjon Asralov"
+from random import randint
 
-import random
-def valid_answer(ask_user):
-    ask_user = ask_user.lower()
-    if ask_user == 'yes':
-        return True
-    else:
-        return False
+def getUserInput() -> int:
+    try:
+        userInput = int(input("Please Enter Your Number: "))
+        return userInput
+    except ValueError:
+        print("Please Enter a Valid Number: ")
+        return -1
 
-def get_user_number():
-    user_num = int(input("Choose a number between 0 and 20 (it must be an integer)\n"))
-    return user_num
+def getTheRandomNumber(userRange) -> int:
+    return randint(0, userRange+1)
 
-def get_comp_number():
-    rand_num = random.randint(0, 21)
-    return rand_num
-
-def compare_numbers(user, computer):
-    if user == computer:
-        return True
-    return False
-
-def main():
-    print('| - - - Welcome to "Number Guesser" game! - - - |')
-    ask_user = input('Do you want to start playing this game? (Yes or No)\n')
-    user_wants_to_continue = True
-    while user_wants_to_continue:
-        if valid_answer(ask_user):
-            user = get_user_number()
-            comp = get_comp_number()
-            if compare_numbers(user, comp):
-                print('Your number is', user)
-                print('Computer\'s number is', comp)
-                print('Congratulations, you guessed the number!')
-            else:
-                print('Your number is', user)
-                print('Computer\'s number is', comp)
-                print('Sorry, you did not guess the number!')
-            ask_again = input('Would you like to play this game again? (Yes or No)\n')
-            ask_again = ask_again.lower()
-            if ask_again == 'yes':
-                user_wants_to_continue = True
-            else:
-                print('Sorry for that! Maybe next time you would like to play this game :)')
-                user_wants_to_continue = False
-
+def getRange() -> int:
+    try:
+        userInputRange = int(input("Please Enter The Range You Would Like To Have Numbers: "))
+        if not (isValidRange(userInputRange)):
+            print("Please Enter A Valid Positive Integer Greater Than 0")
+            return -1
         else:
-            print('Sorry for that! Maybe next time you would like to play this game :)')
-main()
+            return userInputRange
+    except ValueError:
+        print("Please Enter A Valid Positive Integer Greater Than 0")
+        return -1
+
+def isValidRange(n:int) -> bool:
+    return type(n)==int and n > 0
+
+def startGame() -> None:
+    welcomingText = "    Welcome To The Number Guessing Game    "
+    name = "    Abrorjon Asralov    "
+    date = "    9/30/2023    "
+    print()
+    print("+"+"="*(len(welcomingText)+2)+"+")
+    print("||"+" "*(len(welcomingText))+"||")
+    print("||"+welcomingText+"||")
+    print("||"+" "*(len(welcomingText))+"||")
+    print("+"+"="*(len(welcomingText)+2)+"+")
+    print("||"+" "*(len(name))+"||"+" "*len(date)+"||")
+    print("||"+name+"||"+date+"||")
+    print("||"+" "*(len(name))+"||"+" "*len(date)+"||")
+    print("+"+"="*(len(welcomingText)+2)+"+")
+    print()
+
+def isGuessed(user, rand) -> None:
+    if (user == rand):
+        print("Yay you have guessed the Number!!!")
+    elif (user > rand):
+        print("It is higher than expected, please choose a smaller value")
+    elif (user < rand):
+        print("It is smaller than expected, please take a guess with a higher value")
+
+if __name__ == "__main__":
+    startGame()
+    userPreferredRange = getRange()
+    userInput = None
+    while (userPreferredRange==-1 or userInput==-1):
+        if (userInput==-1):
+            userInput = getUserInput()
+        if (userPreferredRange==-1):
+            userPreferredRange = getRange()
+    randomNum = getTheRandomNumber(userPreferredRange)
+    while (userInput!=randomNum):
+        userInput = getUserInput()
+        isGuessed(userInput, randomNum)
